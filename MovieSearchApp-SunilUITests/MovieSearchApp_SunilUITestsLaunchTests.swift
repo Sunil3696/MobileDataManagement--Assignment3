53ba69053c34e250bc30485a22dce541a74bf257
sunil3696 <sunilstark3696@gmail.com>
2024-07-25 02:18:57 -0400

    Initial Commit

---
+++ MovieSearchApp-SunilUITests/MovieSearchApp_SunilUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieSearchApp_SunilUITestsLaunchTests.swift
+//  MovieSearchApp-SunilUITests
+//
+//  Created by Sunil Balami on 2024-07-25.
+//
+
+import XCTest
+
+final class MovieSearchApp_SunilUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
