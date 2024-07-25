e3ab38cc5ec72443c6bde4a0601e1b91cdbaca2a
Sunil3696 <59248750+Sunil3696@users.noreply.github.com>
2024-07-25 05:10:24 -0400

    Delete MovieSearchApp-Sunil directory

--- MovieSearchApp-Sunil/DetailViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DetailViewController.swift
-//  MovieSearchApp-Sunil
-//
-//  Created by Sunil Balami on 2024-07-25.
-//
-
-import Foundation
--- MovieSearchApp-Sunil/SearchViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SearchViewController.swift
-//  MovieSearchApp-Sunil
-//
-//  Created by Sunil Balami on 2024-07-25.
-//
-
-import Foundation
