842a9af1402fc73373eab3e8047f325e1129b0d0
sunil3696 <sunilstark3696@gmail.com>
2024-07-25 03:55:27 -0400

    first commit

---
+++ MovieSearchApp-Sunil/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  MovieSearchApp-Sunil
+//
+//  Created by Sunil Balami on 2024-07-25.
+//
+
+import Foundation
---
+++ MovieSearchApp-Sunil/SearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewController.swift
+//  MovieSearchApp-Sunil
+//
+//  Created by Sunil Balami on 2024-07-25.
+//
+
+import Foundation
